f7c701c83156b176d08c9ab27bae486f8608da05
Ľuboš Mudrák <lubosmudrak93@gmail.com>
2021-01-02 19:56:56 +0100

    update from MASTER

--- src/lib.rs
+++ src/lib.rs
@@ -149,12 +149,12 @@ impl GlutinWindow {
             events: VecDeque::new(),
         })
     }
-    
+
     /// Creates a game window from a pre-existing Glutin event loop and window builder.
     pub fn from_raw(settings: &WindowSettings, event_loop: glutin::event_loop::EventLoop<UserEvent>, window_builder: glutin::window::WindowBuilder) -> Result<Self, Box<dyn Error>> {
         let title = settings.get_title();
         let exit_on_esc = settings.get_exit_on_esc();
-        
+
         let context_builder = context_builder_from_settings(&settings)?;
         let ctx = context_builder.build_windowed(window_builder, &event_loop)?;
         let ctx = unsafe { ctx.make_current().map_err(|(_, err)| err)? };
